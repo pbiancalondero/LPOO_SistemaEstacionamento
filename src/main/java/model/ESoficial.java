327c863bac52e400c99b0205a0a412ad3535ac20
paulalondero <112651841+paulalondero@users.noreply.github.com>
2024-11-03 20:27:09 -0300

    Última atualização.

--- src/main/java/model/ESoficial.java
+++ src/main/java/model/ESoficial.java
@@ -1,19 +1,33 @@
+package model;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package model;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
 
 /**
  *
  * @author paula
  */
-public class ESoficial extends EntradaSaída{
+@Entity
+@DiscriminatorValue("veiculo_oficial")
+public class ESoficial extends EntradaSaida{
+    
+    
     private int kmRegistrado;
+    
+    @OneToOne
+    @JoinColumn(name = "es_oficial_motorista")
     private Pessoa motorista;
 
-    public ESoficial(Tipo tipo, Veículo veículo, Pessoa motorista) {
-        super(tipo, veículo);
+    public ESoficial(TipoMovimentacao tipo, Veiculo veiculo, Pessoa motorista) {
+        super(tipo, veiculo);
         this.motorista = motorista;
     }
 
